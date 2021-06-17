45efa6ac5a504916ef28a2056627aa0e8c33603f
rohansolse <rohansolse@gmail.com>
2021-06-17 16:25:34 +0530

    31 done

--- 3. Matchers/start/calculator.spec.js
+++ 3. Matchers/start/calculator.spec.js
@@ -1,12 +1,12 @@
-describe('calculator.js', function() {
-  it('should add numbers to total', function() {
+describe('calculator.js', function () {
+  it('should add numbers to total', function () {
     const calculator = new Calculator();
     calculator.add(5);
 
     expect(calculator.total).toBe(5);
   });
 
-  it('should subtract numbers from total', function() {
+  it('should subtract numbers from total', function () {
     const calculator = new Calculator();
     calculator.total = 30;
     calculator.subtract(5);
@@ -14,7 +14,7 @@ describe('calculator.js', function() {
     expect(calculator.total).toBe(25);
   });
 
-  it('should multiply total by number', function() {
+  it('should multiply total by number', function () {
     const calculator = new Calculator();
     calculator.total = 100;
     calculator.multiply(2);
@@ -22,11 +22,62 @@ describe('calculator.js', function() {
     expect(calculator.total).toBe(200);
   });
 
-  it('should divide total by number', function() {
+  it('should divide total by number', function () {
     const calculator = new Calculator();
     calculator.total = 200;
     calculator.divide(2);
 
     expect(calculator.total).toBe(100);
   });
-});
+
+  it("has constructor", function () {
+    const calculator1 = new Calculator();
+    const calculator2 = new Calculator();
+    // expect(calculator1).toBe(calculator2)
+    expect(calculator1).toEqual(calculator2)
+    // expect(calculator1).toBeTruthy()
+  })
+  it("has tobetruthy", function () {
+    const calculator1 = new Calculator();
+    const calculator2 = new Calculator();
+    expect(calculator1).toBeTruthy()
+    expect(calculator2).toBeTruthy()
+  })
+
+  it("has unique object", function () {
+    const calculator1 = new Calculator();
+    const calculator2 = new Calculator();
+    expect(calculator1).not.toBe(calculator2)
+  });
+
+  it("has common operations", function () {
+    const calculator = new Calculator();
+    expect(calculator.add).toBeDefined()
+    expect(calculator.add).not.toBeUndefined()
+  });
+
+  it("it can overwrite total", function () {
+    const calculator = new Calculator();
+    calculator.total = null
+    expect(calculator.total).toBeNull()
+  });
+
+  it("it should containes", function () {
+    const calculator = new Calculator();
+    expect(calculator.constructor.name).toContain('Calcu')
+  });
+
+  it("does not handle NaN", function () {
+    const calculator = new Calculator();
+    calculator.total = 20
+    expect(calculator.multiply('a')).toBeNaN()
+  });
+
+  it("handle devide by 0", function () {
+    const calculator = new Calculator();
+    expect(function () { calculator.divide(0) }).toThrow()
+    expect(function () { calculator.divide(0) }).toThrowError(Error)
+    expect(function () { calculator.divide(0) }).toThrowError(Error, "Cannot divide by zero")
+  });
+
+})
---
+++ test.js
@@ -0,0 +1,7 @@
+console.log(!!true);
+console.log(!!null);
+console.log(!!false);
+console.log(!!undefined);
+console.log(!![]);
+console.log(!!{});
+console.log(!!NaN);
